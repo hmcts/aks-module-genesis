e28505e620557d6492646cacdd3547942a098854
Constantin Preda <constantin.preda@hmcts.net>
2019-11-13 13:13:06 +0000

    vars

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "devops_key_vault" {
+  type = "string"
+}
+
+variable "remote_state_storage_account_name" {
+  type = "string"
+}
+
+variable "enable_debug" {
+  type = "string"
+}
+
+variable "deploy_environment" {
+  type = "string"
+}
+
+variable "tag_environment" {
+  type = "string"
+}
+
+variable "acr_private_repo" {
+  type = "string"
+}
+
+variable "acr_public_repo" {
+  type = "string"
+}
