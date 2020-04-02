3daeff8ede3e072fb8cf177bcbdb424c32cdb8ae
stravze <stravze@gmail.com>
2020-04-02 15:51:04 +0100

    fixing variable lookup

--- 01-inputs-required.tf
+++ 01-inputs-required.tf
@@ -1,1 +1,12 @@
 
+variable "devops_key_vault" {
+  type = "string"
+}
+
+variable "acr_private_repo" {
+  type = "string"
+}
+
+variable "acr_public_repo" {
+  type = "string"
+}
--- 40-container-registry.tf
+++ 40-container-registry.tf
@@ -3,7 +3,15 @@
 #--------------------------------------------------------------
 
 resource "azurerm_container_registry" "infrastructure_container_registry" {
-  name                = "petinfrastructure"
+  name                = var.acr_private_repo
+  resource_group_name = azurerm_resource_group.genesis_resource_group.name
+  location            = var.location
+  admin_enabled       = "true"
+  sku                 = "Standard"
+}
+
+resource "azurerm_container_registry" "infrastructure_container_registry" {
+  name                = var.acr_public_repo
   resource_group_name = azurerm_resource_group.genesis_resource_group.name
   location            = var.location
   admin_enabled       = "true"
