a37a915b7f6c40632e32d2cd24296233db9499bb
stravze <stravze@gmail.com>
2020-04-03 13:24:32 +0100

    remove docker storage account output as nolonger required

--- 29-storage-outputs.tf
+++ 29-storage-outputs.tf
@@ -3,7 +3,6 @@ data "null_data_source" "storage_outputs" {
     genesis_resource_group         = azurerm_resource_group.genesis_resource_group.name
     terraform_storage_account_id   = azurerm_storage_account.terraform_storage_account.id
     terraform_storage_container_id = azurerm_storage_container.terraform_storage_container.id
-    docker_storage_account_id      = azurerm_storage_account.docker_storage_account.id
   }
 }
 
