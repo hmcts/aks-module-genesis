791b64fd2266bdeda246e63cc9fc1f903e59877f
stravze <stravze@gmail.com>
2020-04-08 16:06:19 +0100

    fixing new_genesis

--- 00-init.tf
+++ 00-init.tf
@@ -1,7 +1,4 @@
 terraform {
   required_version = ">= 0.12.0"
-  
-  backend "azurerm" {
-    subscription_id  = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
-  }
+
 }
--- 10-resource-group.tf
+++ 10-resource-group.tf
@@ -4,13 +4,26 @@
 
 # Looking up Resource Group in hmcts-control
 
-data "azurerm_resources" "hmcts_control_resource_group" {
+data "azurerm_resource_group" "hmcts_control_resource_group" {
   resource_group_name = "azure-control-sbox-rg"
 }
 
 output "value" {
-  value = data.azurerm_resource_group.name
+  value = data.azurerm_resource_group.hmcts_control_resource_group.name
 }
+
+data "azurerm_storage_account" "terraform_storage_account" {
+  name                = var.hmcts_access_vault
+  resource_group_name = "azure-control-${var.deploy_environment}-rg"
+}
+
+resource "azurerm_key_vault_secret" "terraform_remote_state_storage_account_access_key" {
+  name         = "terraform-remote-state-storage-account-access-key"
+  value        = data.azurerm_storage_account.terraform_storage_account.primary_access_key
+  key_vault_id = azurerm_key_vault.key_vault.id
+}
+
+
 
 # resource "azurerm_resource_group" "genesis_resource_group" {
 #   location = var.location
