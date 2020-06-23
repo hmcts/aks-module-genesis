682f746023df25506894ad5a41b68762afa1d1a5
stravze <stravze@gmail.com>
2020-06-23 13:52:15 +0100

    adding operations groups to keyvault

--- 20-key-vault.tf
+++ 20-key-vault.tf
@@ -4,7 +4,7 @@
 
 
 resource "azurerm_key_vault" "key_vault" {
-  name                = "${element(split("-", data.azurerm_subscription.current.display_name),2)}-vault-${element(split("-", data.azurerm_subscription.current.display_name),3)}"
+  name                = vault-${element(split("-", data.azurerm_subscription.current.display_name),3)}"
   resource_group_name = azurerm_resource_group.genesis_resource_group.name
   location            = var.location
 
@@ -28,6 +28,54 @@ resource "azurerm_key_vault_access_policy" "key_vault_access_policy" {
 
   tenant_id = data.azurerm_client_config.current.tenant_id
   object_id = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "create",
+    "decrypt",
+    "delete",
+    "encrypt",
+    "get",
+    "import",
+    "list",
+    "purge",
+    "recover",
+    "restore",
+    "sign",
+    "unwrapKey",
+    "update",
+    "verify",
+    "wrapKey",
+  ]
+
+  secret_permissions = [
+    "backup",
+    "delete",
+    "get",
+    "list",
+    "purge",
+    "recover",
+    "restore",
+    "set",
+  ]
+}
+
+
+# dcd_sp_ado_${ENV}_operations_v2
+
+
+variable "environment" {
+  default = "test"
+}
+data "azuread_group" "operations_group" {
+  name = "dcd_sp_ado_${var.environment}_operations_v2"
+}
+
+
+resource "azurerm_key_vault_access_policy" "key_vault_access_policy" {
+  key_vault_id = azurerm_key_vault.key_vault.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azuread_group.operations_group.id
 
   key_permissions = [
     "create",
