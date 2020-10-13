1bd3b446da99e717719676888184ac96286c0dbd
Tim Jacomb <tim.jacomb@hmcts.net>
2020-10-13 14:27:39 +0100

    Update 03-interpolated-defaults.tf

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -2,7 +2,5 @@ data "azurerm_subscription" "current" {}
 data "azurerm_client_config" "current" {}
 
 data "azuread_group" "operations_group" {
-  #name = "dcd_sp_ado_${var.environment}_operations_v2"
   name = "DTS Operations (env:${var.environment})"
 }
-
