2f9eefcc27339498bdcd97b4119749c33d48b514
mokainos <72137340+mokainos@users.noreply.github.com>
2020-11-30 15:51:32 +0000

    using AAD group instead of SP

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -4,3 +4,6 @@ data "azurerm_client_config" "current" {}
 data "azuread_group" "operations_group" {
   name = "DTS Operations (env:${var.environment})"
 }
+data "azuread_group" "aad_group" {
+  name = "DTS Contributors (env:${var.environment})"
+}
