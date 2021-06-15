f62e35fd41bbd25836b39c27312d7b5e029d5248
msl8r <72858053+msl8r@users.noreply.github.com>
2021-06-15 12:48:26 +0100

    Update 03-interpolated-defaults.tf

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -2,14 +2,14 @@ data "azurerm_subscription" "current" {}
 data "azurerm_client_config" "current" {}
 
 data "azuread_group" "operations_group" {
-  name = "DTS Operations (env:${var.environment})"
+  display_name = "DTS Operations (env:${var.environment})"
 }
 data "azuread_group" "aad_group" {
-  name = data.azurerm_subscription.current.display_name == "DTS-SHAREDSERVICESPTL-SBOX" ? "DTS Contributors (sub:dts-sharedservicesptl-sbox)" : "DTS Contributors (sub:dts-sharedservices-${var.environment})"
+  display_name = data.azurerm_subscription.current.display_name == "DTS-SHAREDSERVICESPTL-SBOX" ? "DTS Contributors (sub:dts-sharedservicesptl-sbox)" : "DTS Contributors (sub:dts-sharedservices-${var.environment})"
 }
 data "azuread_group" "platform_group" {
-  name = "DTS Platform Operations"
+  display_name = "DTS Platform Operations"
 }
 data "azuread_group" "developers_group" {
-  name = var.developers_group
+  display_name = var.developers_group
 }
