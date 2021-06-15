e9688c5649e80d9da1336336df90b4a61e139ee5
msl8r <72858053+msl8r@users.noreply.github.com>
2021-06-15 16:09:47 +0100

    Update 03-interpolated-defaults.tf

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -1,12 +1,12 @@
 locals {
+  
   environment = var.environment == "ptlsbox" ? "ptl-sbox" : var.environment
+  aad_group_name = contains(["ptlsbox", "ptl"], var.environment) ? "DTS Contributors (sub:dts-sharedservices${local.environment})" : "DTS Contributors (sub:dts-sharedservices-${local.environment})"
   
   # Temporary until all key vaults have been updated to use the naming convention
   key_vault_name = contains(["ptlsbox", "ptl"], var.environment) ? "dtssds${var.environment}" : "${lower(replace(data.azurerm_subscription.current.display_name, "-", ""))}kv"
   
 }
-
-
 
 data "azurerm_subscription" "current" {}
 data "azurerm_client_config" "current" {}
@@ -15,7 +15,7 @@ data "azuread_group" "operations_group" {
   display_name = "DTS Operations (env:${var.environment})"
 }
 data "azuread_group" "aad_group" {
-  display_name = "DTS Contributors (sub:dts-sharedservices-${local.environment})"
+  display_name = local.aad_group_name
 }
 data "azuread_group" "platform_group" {
   display_name = "DTS Platform Operations"
