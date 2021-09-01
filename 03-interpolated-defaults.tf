c79e9e13da6bf57c6dc3e3941063cd150cdbae28
msl8r <72858053+msl8r@users.noreply.github.com>
2021-09-01 12:17:30 +0100

    revert last change (#12)

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -1,25 +1,15 @@
-locals {
-  
-  sub_env = var.environment == "ptlsbox" ? "ptl-sbox" : var.environment
-  aad_group_name = contains(["ptlsbox", "ptl"], var.environment) ? "DTS Contributors (sub:dts-sharedservices${local.sub_env})" : "DTS Contributors (sub:dts-sharedservices-${local.sub_env})"
-  
-  # Temporary until all key vaults have been updated to use the naming convention
-  key_vault_name = contains(["ptlsbox", "ptl"], var.environment) ? "dtssds${var.environment}" : "${lower(replace(data.azurerm_subscription.current.display_name, "-", ""))}kv"
-  
-}
-
 data "azurerm_subscription" "current" {}
 data "azurerm_client_config" "current" {}
 
 data "azuread_group" "operations_group" {
-  display_name = "DTS Operations (env:${var.environment})"
+  name = "DTS Operations (env:${var.environment})"
 }
 data "azuread_group" "aad_group" {
-  display_name = local.aad_group_name
+  name = "DTS Contributors (sub:dts-sharedservices-${var.environment})"
 }
 data "azuread_group" "platform_group" {
-  display_name = "DTS Platform Operations"
+  name = "DTS Platform Operations"
 }
 data "azuread_group" "developers_group" {
-  display_name = var.developers_group
+  name = var.developers_group
 }
