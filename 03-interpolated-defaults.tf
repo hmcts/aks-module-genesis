2b09a9848719138419fac8be0a89ca417dd1aac1
Darren Bromwich <d.bromwich@kainos.com>
2021-09-01 14:17:49 +0100

    changed platform group to use object id

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -8,7 +8,7 @@ data "azuread_group" "aad_group" {
   display_name = "DTS Contributors (sub:dts-sharedservices-${var.environment})"
 }
 data "azuread_group" "platform_group" {
-  display_name = "DTS Platform Operations"
+  object_id = "e7ea2042-4ced-45dd-8ae3-e051c6551789"
 }
 data "azuread_group" "developers_group" {
   display_name = var.developers_group
