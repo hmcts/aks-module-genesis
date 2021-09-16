7be1f63d364cb7dc8393ba2a3d9232221778f1ad
Darren Bromwich <d.bromwich@kainos.com>
2021-09-16 14:59:39 +0100

    terraform fmt performed

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -1,8 +1,8 @@
 locals {
-  
-  sub_env = var.environment == "ptlsbox" ? "ptl-sbox" : var.environment
+
+  sub_env        = var.environment == "ptlsbox" ? "ptl-sbox" : var.environment
   aad_group_name = contains(["ptlsbox", "ptl"], var.environment) ? "DTS Contributors (sub:dts-sharedservices${local.sub_env})" : "DTS Contributors (sub:dts-sharedservices-${local.sub_env})"
-  
+
 }
 data "azurerm_subscription" "current" {}
 data "azurerm_client_config" "current" {}
@@ -14,7 +14,7 @@ data "azuread_group" "aad_group" {
   display_name = local.aad_group_name
 }
 data "azuread_group" "platform_group" {
-  display_name = "DTS Platform Operations"
+  display_name     = "DTS Platform Operations"
   security_enabled = true
 }
 data "azuread_group" "developers_group" {
