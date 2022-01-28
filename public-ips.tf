001001becc2026c0bd89f03206ccbb80889a65f3
msl8r <72858053+msl8r@users.noreply.github.com>
2022-01-28 15:39:05 +0000

    DTSPO-6478 - fix ip name

--- public-ips.tf
+++ public-ips.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip" "this" {
   for_each = toset(var.public_ip_names)
 
-  name                = var.project != "" ? "${var.project}-${each.value}-${var.environment}-pip" : "${each.value}-${var.environment}-pip"
+  name                = "${var.project}-${each.value}-${var.environment}-pip"
   resource_group_name = azurerm_resource_group.genesis_resource_group.name
   location            = var.location
   allocation_method   = "Static"
