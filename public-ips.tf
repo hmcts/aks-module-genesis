a960e2e84da4d063c84253209eb133fcc3633c05
msl8r <72858053+msl8r@users.noreply.github.com>
2022-01-28 16:18:26 +0000

    DTSPO-6478 add ability to create public ips (#21)
    * DTSPO-6478 - enable ability to create public ips
    
    * DTSPO-6478 - tidy up
    
    * DTSPO-6478 - fix resource group reference
    
    * DTSPO-6478 - fix resource group reference
    
    * DTSPO-6478 - change resource name
    
    * Update public-ips.tf
    
    * DTSPO-6478 - fix ip name
    
    * DTSPO-6478 - fix ip name
    
    * DTSPO-6478 - fix ip name
    
    * Update 02-inputs-optional.tf

---
+++ public-ips.tf
@@ -0,0 +1,10 @@
+resource "azurerm_public_ip" "this" {
+  for_each = toset(var.public_ip_names)
+
+  name                = "${var.project}-${each.value}-${var.environment}-pip"
+  resource_group_name = azurerm_resource_group.genesis_resource_group.name
+  location            = var.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = var.tags
+}
