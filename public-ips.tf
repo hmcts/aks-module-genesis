75ec9fde75cca9f960eb8bd21e90d0f237360949
msl8r <72858053+msl8r@users.noreply.github.com>
2022-01-28 11:52:05 +0000

    DTSPO-6478 - fix resource group reference

--- public-ips.tf
+++ public-ips.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "public-lb" {
   for_each = toset(var.public_ip_names)
 
   name                = each.value
-  resource_group_name = azurerm_resource_group.genesis_resource_group
+  resource_group_name = azurerm_resource_group.genesis_resource_group.id
   location            = var.location
   allocation_method   = "Static"
   sku                 = "Standard"
