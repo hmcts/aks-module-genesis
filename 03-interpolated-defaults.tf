a9b43a69888288153c6731516ef9fdd266615546
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-06-05 14:42:49 +0100

    update condition

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -2,12 +2,8 @@ locals {
 
   sub_env        = var.environment == "ptlsbox" ? "ptl-sbox" : var.environment
   
-  cft_aad_group_name = var.environment == "ptl" && contains(["cft"], data.azurerm_subscription.current.display_name) ? "DTS Contributors (sub:dts-cftptl-intsvc" : (var.environment == "ptlsbox" && contains(["cft"], data.azurerm_subscription.current.display_name) ? "DTS Contributors (sub:dts-cftsbox-intsvc)" : "DTS Contributors (sub:dts-cftapps-${local.sub_env})")
+  aad_group_name = var.environment == "ptl" && contains(["cft"], data.azurerm_subscription.current.display_name) ? "DTS Contributors (sub:dts-cftptl-intsvc)" : (var.environment == "ptlsbox" && contains(["cft"], data.azurerm_subscription.current.display_name) ? "DTS Contributors (sub:dts-cftsbox-intsvc)" : var.environment == "ptl" && contains(["sharedservices"], data.azurerm_subscription.current.display_name) ? "DTS Contributors (sub:dts-sharedservicesptl)" : (var.environment == "ptlsbox" && contains(["sharedservices"], data.azurerm_subscription.current.display_name) ? "DTS Contributors (sub:dts-sharedservicesptl-sbox)" : (!contains(["ptl","ptlsbox"], var.environment) && contains(["sharedservices"], data.azurerm_subscription.current.display_name) ? "DTS Contributors (sub:dts-sharedservicesptl-sbox)" : "DTS Contributors (sub:dts-cftapps-${local.sub_env})")))
 
-  sds_aad_group_name = var.environment == "ptl" && contains(["sharedservices"], data.azurerm_subscription.current.display_name) ? "DTS Contributors (sub:dts-sharedservicesptl" : (var.environment == "ptlsbox" && contains(["sharedservices"], data.azurerm_subscription.current.display_name) ? "DTS Contributors (sub:dts-sharedservicesptl-sbox)" : "DTS Contributors (sub:dts-sharedservices-${local.sub_env})")
-
-  aad_group_name = contains(["cft"], data.azurerm_subscription.current.display_name) ? local.cft_aad_group_name : local.sds_aad_group_name
-  
   project = length(regexall(".*sds.*", lower(var.developers_group))) > 0 ? "sds" : "cft"
 
 }
