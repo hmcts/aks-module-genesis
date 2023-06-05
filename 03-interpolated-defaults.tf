65c314c4b605e045b85ff9eabd1b13436374cd89
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-06-05 14:17:26 +0100

    update locals

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -1,8 +1,12 @@
 locals {
 
   sub_env        = var.environment == "ptlsbox" ? "ptl-sbox" : var.environment
-  aad_group_name = contains(["ptlsbox", "ptl"], var.environment) ? "DTS Contributors (sub:dts-sharedservices${local.sub_env})" : "DTS Contributors (sub:dts-sharedservices-${local.sub_env})"
+  
+  cft_aad_group_name = var.environment == "ptl" && contains(["cft"], data.azurerm_subscription.current.display_name) ? "DTS Contributors (sub:dts-cftptl-intsvc" : (var.environment == "ptlsbox" && contains(["cft"], data.azurerm_subscription.current.display_name) ? "DTS Contributors (sub:dts-cftsbox-intsvc)" : "DTS Contributors (sub:dts-cftapps-${local.sub_env})")
 
+  sds_aad_group_name = var.environment == "ptl" && contains(["sharedservices"], data.azurerm_subscription.current.display_name) ? "DTS Contributors (sub:dts-sharedservicesptl" : (var.environment == "ptlsbox" && contains(["sharedservices"], data.azurerm_subscription.current.display_name) ? "DTS Contributors (sub:dts-sharedservicesptl-sbox)" : "DTS Contributors (sub:dts-sharedservices-${local.sub_env})")
+
+  aad_group_name = contains(["cft"], data.azurerm_subscription.current.display_name) ? local.cft_aad_group_name : local.sds_aad_group_name
   
   project = length(regexall(".*sds.*", lower(var.developers_group))) > 0 ? "sds" : "cft"
 
