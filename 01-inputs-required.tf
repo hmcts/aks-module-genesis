06cc3fa04ffc9d738518c0983fe93c3c501b4775
dawidstrozak <123395719+dawidstrozak@users.noreply.github.com>
2026-04-27 11:08:39 +0100

    Merge pull request #35 from hmcts/dtspo-30482-grant-per-env-jenkins-mi-on-kv
    Dtspo 30482 grant per env jenkins mi on kv created by this module

--- 01-inputs-required.tf
+++ 01-inputs-required.tf
@@ -1,3 +1,6 @@
 variable "environment" {}
 variable "developers_group" {}
 variable "business_area" {}
+variable "jenkins_provider_sub_id" {}
+variable "jenkins_mi_name" {}
+variable "jenkins_mi_rg_name" {}
--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -11,11 +11,18 @@ locals {
     "sds-ptl" = "DTS Contributors (sub:dts-sharedservicesptl)"
     "sds-ptlsbox" = "DTS Contributors (sub:dts-sharedservicesptl-sbox)"
     "sds-env" = "DTS Contributors (sub:dts-sharedservices-${local.sub_env})"
-    }
+  }
 
   business_area = var.business_area == "cross-cutting" ? "sds" : var.business_area
 
 }
+
+provider "azurerm" {
+  alias           = "jenkins-azurerm"
+  features {}
+  subscription_id = var.jenkins_provider_sub_id
+}
+
 data "azurerm_subscription" "current" {}
 data "azurerm_client_config" "current" {}
 
@@ -31,4 +38,10 @@ data "azuread_group" "platform_group" {
 }
 data "azuread_group" "developers_group" {
   display_name = var.developers_group
+}
+
+data "azurerm_user_assigned_identity" "jenkins" {
+  provider            = azurerm.jenkins-azurerm
+  name                = var.jenkins_mi_name
+  resource_group_name = var.jenkins_mi_rg_name
 }
