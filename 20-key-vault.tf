aaed109ed371976229c2f83aac867301767f4e2f
Daniel Alves <daniel.alves@justice.gov.uk>
2026-05-27 11:52:56 +0100

    feat: enable kv purge protection

--- 20-key-vault.tf
+++ 20-key-vault.tf
@@ -15,6 +15,7 @@ resource "azurerm_key_vault" "key_vault" {
   enabled_for_deployment          = "true"
   enabled_for_disk_encryption     = "true"
   enabled_for_template_deployment = "true"
+  purge_protection_enabled        = var.purge_protection_enabled
   soft_delete_retention_days      = 30
 
   tags = var.tags
@@ -148,4 +149,3 @@ resource "azurerm_key_vault_access_policy" "platform_operations" {
     "Update"
   ]
 }
-
